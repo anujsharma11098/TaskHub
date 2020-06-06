7b081c9621c26eb1910abce118f897920ea12f16
Anuj Sharma <48546869+anujsharma11098@users.noreply.github.com>
2020-06-06 18:19:49 +0530

    up

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
 module.exports = async (req, res, next) => {
-    const token = req.headers.authorization
+    const token = req.body.Authorization
     if (!token) return res.status(401).json({ status: 401, message: 'Must send Authorization token' })
     try {
         const result = await jwt.verify(token, process.env.JWT_SECRET)
